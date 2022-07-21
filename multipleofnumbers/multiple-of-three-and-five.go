499aa7bab241d7a9a239376b9e2d9ef165830e34
GaijinZ <tomasz.mostowy@gmail.com>
2022-07-21 12:41:21 +0200

    package multiple of three and not five

---
+++ multipleofnumbers/multiple-of-three-and-five.go
@@ -0,0 +1,13 @@
+package multipleofnumbers
+
+func MultipleOfThreeaAndFive(arr []int) []int {
+	finalNumbers := []int{}
+
+	for _, element := range arr {
+		if element%3 == 0 && element%5 != 0 {
+			finalNumbers = append(finalNumbers, element)
+		}
+	}
+
+	return finalNumbers
+}
